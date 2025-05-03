5236178a86f5a3c5f7c8e88380e169b21dbc2560
y1u0d2a1i <y.kotani@raksul.com>
2025-05-03 17:58:27 +0900

    init mastra

---
+++ my-mastra-app/src/mastra/index.ts
@@ -0,0 +1,18 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { createLogger } from '@mastra/core/logger';
+import { LibSQLStore } from '@mastra/libsql';
+
+import { weatherAgent } from './agents';
+
+export const mastra = new Mastra({
+  agents: { weatherAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: createLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+});
